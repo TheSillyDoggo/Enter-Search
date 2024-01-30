ca6f660ff3e949bf42e3ea3263c2f0ad16f818ea
Explodingbill <talgood09@gmail.com>
2024-01-30 12:42:52 +1100

    my bindings

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,6 @@
 
 using namespace geode::prelude;
 
-#ifdef GEODE_IS_WINDOWS
-
 class $modify (FLAlertLayer)
 {
 	bool init(FLAlertLayerProtocol* p0, char const* p1, gd::string p2, char const* p3, char const* p4, float p5, bool p6, float p7, float p8)
@@ -19,8 +17,6 @@ class $modify (FLAlertLayer)
 		return true;
 	}
 };
-
-#endif
 
 class $modify (LevelSearchLayer)
 {	
